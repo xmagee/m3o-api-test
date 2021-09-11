3686d7111bb40ff4a55a3524726d676ff1ee2fae
Alex Magee <86931568+xmagee@users.noreply.github.com>
2021-09-10 23:06:45 -0400

    corrected the formatting

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 require('dotenv').config()
 const m3o = require('@m3o/m3o-node'),
-  client = new m3o.Client({ token: process.env.MICRO_API_KEY })
+	client = new m3o.Client({ token: process.env.MICRO_API_KEY })
 
 async function main() {
-    
-    client.call('helloworld', 'Call', {
-        "name": "alex"
-      })
-        .then(response => {
-          console.log(response);
-        });
+
+	client.call('helloworld', 'Call', {
+		"name": "alex"
+	})
+		.then(response => {
+			console.log(response);
+		});
 
 }
 
